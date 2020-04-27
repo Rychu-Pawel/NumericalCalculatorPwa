8d57c2a34219ebd0108b9447845f76297c89d652
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2020-04-27 21:39:12 +0200

    Working on mean; Upgraded to preview 5; Minor visual tweaks (changed menu breaking step);

---
+++ NumericalCalculatorPwa/Pages/Mean/ViewModels/MeanViewModel.cs
@@ -0,0 +1,10 @@
+﻿using NumericalCalculatorPwa.ViewModels;
+
+namespace NumericalCalculatorPwa.Pages.Mean.ViewModels
+{
+    public class MeanViewModel
+    {
+        public ValueErrorModel Value { get; set; } = new ValueErrorModel();
+        public ValueErrorModel Weight { get; set; } = new ValueErrorModel();
+    }
+}
