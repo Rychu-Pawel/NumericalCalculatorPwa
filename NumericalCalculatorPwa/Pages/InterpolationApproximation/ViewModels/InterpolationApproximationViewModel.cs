ddcbbfa185802b08ed6f2d8f2ee93afc1629cb85
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2020-05-31 23:28:38 +0200

    Working on interpolation and approximation

---
+++ NumericalCalculatorPwa/Pages/InterpolationApproximation/ViewModels/InterpolationApproximationViewModel.cs
@@ -0,0 +1,10 @@
+﻿using NumericalCalculatorPwa.ViewModels;
+
+namespace NumericalCalculatorPwa.Pages.InterpolationApproximation.ViewModels
+{
+    public class InterpolationApproximationViewModel
+    {
+        public ValueErrorModel X { get; set; } = new ValueErrorModel();
+        public ValueErrorModel Y { get; set; } = new ValueErrorModel();
+    }
+}
