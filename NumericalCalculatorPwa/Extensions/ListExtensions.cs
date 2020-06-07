9cdad827e037421abefc5d776f87ce7cb056a32e
pawel.rychlicki@gmail.com <pawel.rychlicki@gmail.com>
2020-06-07 22:45:55 +0200

    Started work on LinearEquations

---
+++ NumericalCalculatorPwa/Extensions/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace NumericalCalculatorPwa.Extensions
+{
+    public static class ListExtensions
+    {
+        public static List<T> Populate<T>(this List<T> list) where T : new()
+        {
+            for (int i = 0; i < list.Count; i++)
+                list[i] = new T();
+
+            return list;
+        }
+    }
+}
